5b560c6561b37edb0e86a77cf53ad6914b12a46d
Jeb.Wang <jeb.wannng@gmail.com>
2023-08-31 19:23:18 +0800

    add tsconfig.json

---
+++ dist/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hello world!');
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+greet("Maddison", new Date());
+let b = 'hello world';
+b = 1;
---
+++ src/hello.ts
@@ -0,0 +1,8 @@
+console.log('Hello world!');
+
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Maddison", new Date());
+
